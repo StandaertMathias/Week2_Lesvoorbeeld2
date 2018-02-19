eba03864879f1735763d1d45ea1ce2ca43f85429
Mathias Standaert <mathias.standaert@student.howest.be>
2018-02-19 11:05:05 +0100

    Add project files.

---
+++ Week2_Lesvoorbeeld2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Week2_Lesvoorbeeld2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
